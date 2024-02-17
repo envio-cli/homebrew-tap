bbbd6ad023ed2a759e22d5967b842045bde02f6c
Humble Penguin <humblepenguinn@gmail.com>
2024-02-17 08:05:48 +0500

    Update version v0.5.0 -> v0.5.1

--- Formula/envio.rb
+++ Formula/envio.rb
@@ -1,9 +1,9 @@
 class Envio < Formula
   desc "A Modern And Secure CLI Tool For Managing Environment Variables"
   homepage "https://envio-cli.github.io/home"
-  url "https://github.com/envio-cli/envio/releases/download/v0.5.0/envio-v0.5.0-x86_64-apple-darwin.tar.gz"
-  sha256 "2726b43858ad2eceff1485839ba856e515b5052212c9a091af0b28244db9927a"
-  version "0.5.0"
+  url "https://github.com/envio-cli/envio/releases/download/v0.5.1/envio-v0.5.1-x86_64-apple-darwin.tar.gz"
+  sha256 "ff6bf431c8a35fb75b178efadba9aa7d77e0d841c57f76cd0fc59df49b30dbb6"
+  version "0.5.1"
 
   def install
     bin.install "envio"
